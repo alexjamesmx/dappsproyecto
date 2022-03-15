6bd70d822e57925ee415766ab7c1098902ee3c6c
alexjamesmx <sg.alex32@gmail.com>
2022-03-15 12:47:41 -0600

    validaciones

--- app/src/main/java/com/example/calistheniks/registerActivity.java
+++ app/src/main/java/com/example/calistheniks/registerActivity.java
@@ -6,39 +6,191 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.calistheniks.databinding.ActivityRegisterBinding;
+
+import java.util.regex.Pattern;
+
 public class registerActivity extends AppCompatActivity {
     private ActionBar actionBar;
     private Button btnregistrocatalogo;
+    private ActivityRegisterBinding binding;
 
-    //private Binding registerActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         btnregistrocatalogo = findViewById(R.id.btn_register_catalog);
+        binding.registerTietFirstname.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+        binding.registerTietLastname.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+        binding.registerTietHomeaddress.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
 
-        //USO DE ACTION BAR PARA VOLVER UN SCREEN ATRAS
+        //**************ACTION BAR PARA VOLVER UN SCREEN ATRAS******************************
         actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-        //AL PULSAR EL BOTON REGISTARSE NAVEGAMOS AL CATALOGO
+        //********************************************
+
+        //Registrarse-a-catalogo
         btnregistrocatalogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(
-                        new Intent(
-                                registerActivity.this,
-                                Catalogo_Activity.class
-                        )
-                );
+                if(validarCorreo(binding.registerTietEmail.getText().toString())){
+                    startActivity(
+                            new Intent(
+                                    registerActivity.this,
+                                    Catalogo_Activity.class
+                            )
+                    );
+                }
             }
         });
+
+        binding.registerTietFirstname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                validarFirstName(editable.toString());
+            }
+        });
+
+        binding.registerTietLastname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                validarLastName(editable.toString());
+            }
+        });
+
+
     }
+
+
+    public boolean validarFirstName(String texto) {
+        if (texto.trim().length() < 3 ) {
+            binding.registerTilFirstname.setError("just letters and 3 length minimum" );
+            binding.registerTilFirstname.setErrorIconDrawable(null);
+            return false;
+        }
+        else if(texto.trim().length() > 20 ){
+            binding.registerTilFirstname.setError("Username must be 20 length maximum");
+            binding.registerTilFirstname.setErrorIconDrawable(null);
+            return false;
+        } else {
+            binding.registerTilFirstname.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+    public boolean validarLastName(String texto) {
+        if (texto.trim().length() < 3 ) {
+            binding.registerTilLastname.setError("just letters and 3 length minimum" );
+            binding.registerTilLastname.setErrorIconDrawable(null);
+            return false;
+        }
+        else if(texto.trim().length() > 20 ){
+            binding.registerTilLastname.setError("Username must be 20 length maximum");
+            binding.registerTilLastname.setErrorIconDrawable(null);
+            return false;
+        } else {
+            binding.registerTilLastname.setErrorEnabled(false);
+            return true;
+        }
+    }
+    public boolean validarCorreo(String texto){
+        if(Patterns.EMAIL_ADDRESS.matcher(texto).matches()){
+            binding.registerTilEmail.setErrorEnabled(false);
+            return true;
+        }
+        else{
+            binding.registerTilEmail.setError("Enter a valid email");
+            binding.registerTilEmail.setErrorIconDrawable(null);
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     //USO DE ACTION BAR PARA VOLVER UN SCREEN ATRAS
--- app/src/main/java/com/example/calistheniks/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/calistheniks/ui/login/LoginFragment.java
@@ -2,10 +2,15 @@ package com.example.calistheniks.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,10 +23,16 @@ import com.example.calistheniks.registerActivity;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+
+import java.util.Objects;
+
 //todos los fragmentos necesitan de un onCreate y un onCreateView
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
+
+    boolean isAllFieldsChecked = false, isAllFieldsFilled = false;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,6 +40,41 @@ public class LoginFragment extends Fragment {
         binding = FragmentLoginBinding.inflate(inflater, container, false);
         //Para obtener una referencia a la vista raíz, llama al método getRoot();
         View root = binding.getRoot();
+        binding.loginTietEmail.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+//        binding.loginTietEmail.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//                //   binding.loginTietUsername.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+//                validarNombre(editable.toString());
+//            }
+//        });
+
+        binding.loginTietPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                validarPassword(editable.toString());
+            }
+        });
+
         return root;
     }
 
@@ -38,14 +84,26 @@ public class LoginFragment extends Fragment {
         binding.loginFragmentCatalogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavController navController = Navigation.findNavController(
-                        getActivity(),
-                        R.id.nav_host_fragment_content_catalogo
-                );
-                navController.navigateUp();
-                navController.navigate(R.id.nav_catalogo);
+
+                isAllFieldsChecked = CheckAllFields();
+                isAllFieldsFilled = CheckAllFieldsVoid();
+
+                if (!isAllFieldsFilled && isAllFieldsChecked) {
+                    Toast.makeText(getActivity(), "Fill the form, please", Toast.LENGTH_LONG).show();
+                } else if (isAllFieldsChecked && isAllFieldsFilled &&validarNombre(binding.loginTietEmail.getText().toString())) {
+                    NavController navController = Navigation.findNavController(
+                            getActivity(),
+                            R.id.nav_host_fragment_content_catalogo
+                    );
+                    navController.navigateUp();
+                    navController.navigate(R.id.nav_catalogo);
+                } else {
+                    Toast.makeText(getActivity(), "Something's wrong", Toast.LENGTH_LONG).show();
+                }
             }
         });
+
+
         //Al pulsar el boton "register" se navega a la actividad register
         binding.loginFragmentRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,4 +119,81 @@ public class LoginFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
+
+//
+//    public boolean validarNombre(String texto) {
+//        if (texto.trim().length() < 3) {
+//            binding.loginTilUsername.setError("Username must be 3 length minimum");
+//            binding.loginTilUsername.setErrorIconDrawable(null);
+//            return false;
+//        }
+////        else if(texto.trim().length() > 6 ){
+////            binding.loginTilUsername.setError("Username must be 6 length");
+////            binding.loginTilUsername.setErrorIconDrawable(null);
+////            return false;
+////        }
+//        else {
+//            binding.loginTilUsername.setError("");
+//            binding.loginTilUsername.setErrorEnabled(false);
+//            return true;
+//        }
+//    }
+
+    public boolean validarNombre(String texto){
+        if(Patterns.EMAIL_ADDRESS.matcher(texto).matches()){
+            binding.loginTilEmail.setErrorEnabled(false);
+            return true;
+        }
+        else{
+            binding.loginTilEmail.setError("Enter a valid email");
+            binding.loginTilEmail.setErrorIconDrawable(null);
+            return false;
+        }
+    }
+
+
+    public boolean validarPassword(String texto) {
+        if (texto.trim().length() < 8 || texto.contains(" ")) {
+            if (texto.contains(" ")) {
+                binding.loginTilPassword.setError("Dont leave white spaces");
+                return false;
+            } else {
+                binding.loginTilPassword.setError("Password must be 8 length minimum");
+                binding.loginTilPassword.setErrorIconDrawable(null);
+                return false;
+            }
+        } else {
+            binding.loginTilPassword.setError("");
+            binding.loginTilPassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+    private boolean CheckAllFields() {
+        if (binding.loginTilPassword.isErrorEnabled()) {
+            return false;
+        } else if (binding.loginTilEmail.isErrorEnabled()) {
+            return false;
+        } else
+            // after all validation return true.
+            return true;
+    }
+
+    private boolean CheckAllFieldsVoid() {
+        if (binding.loginTietPassword.getText().toString().equals("")) {
+            return false;
+        } else if (binding.loginTietEmail.getText().toString().equals("")) {
+            return false;
+        } else
+            // after all validation return true.
+            return true;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
